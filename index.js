75e15270b63b5510c8186c29c1f56c8eec828990
fanil <demolit_fanil@mail.ru>
2022-05-30 11:01:36 +0000

    11

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const fs = require('fs')
 const path = require('path')
 const radspec = require('radspec')
 const ABI_FOLDER = './abi/'
+// const debug = true
 const debug = false
-// const debug = false
 function Parser(params) {
     this.web3 = params.web3
     this.installedApps = params.installedApps
@@ -13,7 +13,7 @@ function Parser(params) {
         this.votingAddress = params.votingAddress
     } else {
         const votingApp = this.installedApps.find((obj) => { return obj.name == 'voting' })
-        this.votingAddress = '0x' + votingApp.app
+        this.votingAddress = votingApp.app
     }
     const votingAbi = JSON.parse(fs.readFileSync(path.resolve(__dirname, `${ABI_FOLDER}voting.json`)))
     this.voting = new this.web3.eth.Contract(votingAbi.abi, this.votingAddress)
@@ -24,51 +24,87 @@ Parser.prototype.parse = function parse(params) {
     return new Promise(async (resolve, reject) => {
         const voteId = params.voteId
         let call, expression, vote
+
         try {
             vote = params.vote ? params.vote : await this.voting.methods.getVote(voteId).call()
         } catch (error) {
             return reject({ error: 'Can\'t get vote', success: false })
         }
         trace('Vote got')
-        const app = vote.script.replace('0x', '').substring(8, 48)
-        const findApp = this.installedApps.find((obj) => { return obj.app == app })
+        const appAddress = '0x' + vote.script.substring(10, 50)
+
+        const findApp = this.installedApps.find((obj) => { return obj.app == appAddress })
+
         if (findApp == undefined) return reject({ error: 'Unknown app', success: false })
         const appName = findApp.name.replace('-', '')
-        trace(appName)
-
         try {
-            const abiFile = JSON.parse(fs.readFileSync(path.resolve(__dirname, `${ABI_FOLDER}${appName}.json`)))
-            const methodByte = '0x' + vote.script.replace('0x', '').substring(56, 64)
-            const methodName = getMethodName({ web3: this.web3, abi: abiFile, methodByte })
-            expression = abiFile.userdoc.methods[methodName].notice
+            this.abi = JSON.parse(fs.readFileSync(path.resolve(__dirname, `${ABI_FOLDER}${appName}.json`)))
+            this.methodByte = '0x' + vote.script.replace('0x', '').substring(56, 64)
+            trace('methodByte:')
+            trace(this.methodByte)
+            this.getMethodName()
+            trace('methodName:')
+            trace(this.methodName)
+            expression = this.abi.userdoc.methods[this.methodName].notice
+            this.methodCall = '0x' + vote.script.replace('0x', '').substring(56)
+            trace('methodCall:')
+            trace(this.methodCall)
             trace(expression)
             call = {
-                abi: abiFile.abi,
+                abi: this.abi.abi,
                 transaction: {
-                    data: '0x' + vote.script.replace('0x', '').substring(56)
+                    data: this.methodCall
                 }
             }
         } catch {
             return reject({ error: 'Unknown method', success: false })
         }
         try {
-            const message = await radspec.evaluate(expression, call)
-            trace(message)
-            resolve({ message: `${app}: ${message}`, success: true })
+            const radspecMessage = await radspec.evaluate(expression, call)
+            const message = this.replaceAddressWithName(`${appAddress}: ${radspecMessage}`)
+            resolve({ message, success: true })
         } catch (error) {
-            return resolve({ message: `!${app}: ${expression}`, success: true })
+            let message = this.replaceAddressWithName(`${appAddress}: ${expression}`)
+            message = this.replaceParamsValues(message)
+            return resolve({ message, success: true })
         }
 
     })
 }
-function getMethodName(params) {
-    const { web3, abi, methodByte } = params
-    const abiMethods = Object.keys(abi.userdoc.methods)
-    for (const method of abiMethods) {
-        const decodedName = web3.eth.abi.encodeFunctionSignature(method)
-        if (decodedName == methodByte) return method
-    }
-    return null
+Parser.prototype.replaceAddressWithName = function replaceAddressWithName(txt) {
+    for (const app of this.installedApps)
+        txt = txt.replace(app.app, app.name.capitalize())
+    return txt
 }
+Parser.prototype.replaceParamsValues = function replaceParamsValues(txt) {
+    try {
+        const paramsTypes = this.methodName.split('(')[1].split(')')[0].split(',')
+        const paramsNames = Object.keys(this.abi.devdoc.methods[this.methodName].params)
+        const paramsObj = paramsTypes.map((p, i) => ({ type: p, name: paramsNames[i] }))
+        const paramsValues = this.web3.eth.abi.decodeParameters(paramsObj, this.methodCall.substring(10))
+        if (paramsNames && paramsNames.length) {
+            for (const param of paramsNames) {
+                txt = txt.replace(param, paramsValues[param])
+            }
+        }
+    } catch (error) {
+        console.log(error)
+    }
+    return txt
+}
+Parser.prototype.getMethodName = function getMethodName() {
+    const abiMethods = Object.keys(this.abi.userdoc.methods)
+    for (const method of abiMethods) {
+        const decodedName = this.web3.eth.abi.encodeFunctionSignature(method)
+        if (decodedName == this.methodByte) this.methodName = method
+    }
+}
+Object.defineProperty(String.prototype, 'capitalize', {
+    value: function () {
+        return this.charAt(0).toUpperCase() + this.slice(1);
+    },
+    enumerable: false
+});
 function trace(msg) { if (debug) console.log(msg) }
+
 module.exports = Parser
--- test.js
+++ test.js
@@ -7,42 +7,42 @@ const web3 = new Web3(rpc)
 
 
 const installedApps = [
-    { app: 'b104f3133e9a04d9048feb2e26ed1e72036c078c', name: 'acl' },
-    { app: 'b1198586d39f533477104e2bf9186a87785d1bd5', name: 'evmreg' },
-    { app: '8507f9cd4bbe41664865c5cb3e6503e234f67f98', name: 'vault' },
-    { app: 'd422c73fa1df211f232e162746293bff411beaaf', name: 'agent' },
+    { app: '0xb104f3133e9a04d9048feb2e26ed1e72036c078c', name: 'acl' },
+    { app: '0xb1198586d39f533477104e2bf9186a87785d1bd5', name: 'evmreg' },
+    { app: '0x8507f9cd4bbe41664865c5cb3e6503e234f67f98', name: 'vault' },
+    { app: '0xd422c73fa1df211f232e162746293bff411beaaf', name: 'agent' },
     {
-        app: '0f109bf1d4cca3dc64cf0ccb89898445529699f3',
+        app: '0x0f109bf1d4cca3dc64cf0ccb89898445529699f3',
         name: 'dot-voting'
     },
-    { app: '2abe7c3bb9d84560e75bb698697a35fd390093a9', name: 'voting' },
+    { app: '0x2abe7c3bb9d84560e75bb698697a35fd390093a9', name: 'voting' },
     {
-        app: 'c1a8df07504adb6437b923168cb49236b0cbf2b8',
+        app: '0xc1a8df07504adb6437b923168cb49236b0cbf2b8',
         name: 'token-manager',
         token: '0xDaa3867b1b7cc90a6b0586a55098725628A98E60',
         is_gp: true
     },
     {
-        app: 'c0eede8a66398f3f850604e220471b961ac37314',
+        app: '0xc0eede8a66398f3f850604e220471b961ac37314',
         name: 'token-manager',
         token: '0x302B5F75C4016C358A9d09283A13f69400E8D4A6'
     },
     {
-        app: '91a87142a85a4f3afbee01dd9e02135f3eab382d',
+        app: '0x91a87142a85a4f3afbee01dd9e02135f3eab382d',
         name: 'allocations'
     },
-    { app: '08ea318a839df7b5901afdfba7f7b2e31edd8b63', name: 'finance' },
-    { app: '29abc1fe6b5d8933c4b351e9d63c0f43ea4ed3c2', name: 'rewards' },
+    { app: '0x08ea318a839df7b5901afdfba7f7b2e31edd8b63', name: 'finance' },
+    { app: '0x29abc1fe6b5d8933c4b351e9d63c0f43ea4ed3c2', name: 'rewards' },
     {
-        app: 'f27e3077afd927e7188006dcb43dcfeaa91cc3fe',
+        app: '0xf27e3077afd927e7188006dcb43dcfeaa91cc3fe',
         name: 'redemptions'
     },
     {
-        app: '448ed925a5ed0fcb289429e45617f9e4f3298a2f',
+        app: '0x448ed925a5ed0fcb289429e45617f9e4f3298a2f',
         name: 'token-request'
     },
-    { app: '83d4b7581fef427e6bfb61c311a3f31139f25057', name: 'delay' },
-    { name: 'kernel', app: 'fa35141ed7032840f70badb1cc0715a9ced4d966' }
+    { app: '0x83d4b7581fef427e6bfb61c311a3f31139f25057', name: 'delay' },
+    { name: 'kernel', app: '0xfa35141ed7032840f70badb1cc0715a9ced4d966' }
 ]
 
 const weeziVotingParser = new WeeziVotingParser({ web3, installedApps })
